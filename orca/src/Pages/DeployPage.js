eafe0de7f6e9e430c176db9152cec27d618adf30
GavinLau15 <gavinl1546@gmail.com>
2025-03-23 00:27:11 -0700

    Side bar made persistent across pages

--- orca/src/Pages/DeployPage.js
+++ orca/src/Pages/DeployPage.js
@@ -1,3 +1,42 @@
+import { Link } from "react-router-dom";
+import orcaLogo from "./orcaLogo.png";
+
 export function DeployPage() {
-    return <h1>deploy page</h1>;
+    return (
+        <div className="d-flex vh-100">
+            {/* Sidebar */}
+            <aside
+                className="bg-light border-end p-3 d-flex flex-column"
+                style={{ width: "250px" }}
+            >
+                {/* Logo */}
+                <div className="d-flex align-items-center mb-4">
+                    <img
+                        src={orcaLogo}
+                        alt="Orca Logo"
+                        className="me-2"
+                        width="80"
+                    />
+                    <h5 className="mb-0">ORCA</h5>
+                </div>
+
+                {/* Sidebar Menu */}
+                <nav className="d-flex flex-column gap-2">
+                    <h6>Menu</h6>
+                    <Link to="/" className="btn btn-light text-start">
+                        Home
+                    </Link>
+                    <Link to="/jobs" className="btn btn-light text-start">
+                        Jobs
+                    </Link>
+                    <Link to="/data" className="btn btn-light text-start">
+                        Data
+                    </Link>
+                    <Link to="/deploy" className="btn btn-light text-start">
+                        Deploy
+                    </Link>
+                </nav>
+            </aside>
+        </div>
+    );
 }
